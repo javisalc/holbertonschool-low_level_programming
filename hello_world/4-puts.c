5ef4fabd5f099f6ce0d19b2e0aa432584a8d9d66
javier <javisalc@gmail.com>
2023-10-26 08:35:16 -0700

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point of the program.
+ *
+ *this function prints a message to the console.
+ *
+ *Return> Always 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
