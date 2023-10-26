0a111fbb46bb20d4c46d3f3857dfcebe6e81900c
javier <javisalc@gmail.com>
2023-10-26 09:55:38 -0700

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *                a piece of art," followed by a new line.
  *Return: Always 0.
  */
-int (main(void)
+int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
   return (0);
