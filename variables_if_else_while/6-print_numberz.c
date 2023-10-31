1dfd65602e1bab41e1abadcd744de9fd5023ed79
javier <javisalc@gmail.com>
2023-10-31 10:46:01 -0700

    Print numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - prints single digit number
+ *
+ *Return - Always (succsess)
+ */
+int main(void)
+{
+for (int = 0; int <= 9; int++)
+{
+putchar(int);
+}
+putchar("\n");
+return (0);
+}
