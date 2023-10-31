a52ed2af77093e32851d202b6eda432c1621b5bf
javier <javisalc@gmail.com>
2023-10-31 13:04:32 -0700

    Print combination

---
+++ variables_if_else_while/9-print_com.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+ * main - print a combination of numbers
+ *
+ * Return: Always (success)
+ */ /
---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stio.h>
+/**
+ * main - print a combination of numbers using putchar
+ *
+ * return: Always (success)
+ */
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
