ea308016fad6a1024a141802f2398250b43439e7
javier <javisalc@gmail.com>
2023-10-31 10:28:38 -0700

    Print numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,10 @@
 int main(void)
 {
 char number;
- for (number = 0; number <= 9; number++)
+for (number = 0; number <= 9; number++)
 {
-  printf("%d\n", number);
+printf("%d", number);
+printf("\n");
 }
 return (0);
 }
