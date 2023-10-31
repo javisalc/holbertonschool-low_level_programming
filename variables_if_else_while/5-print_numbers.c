a4a88add87266e9828dc5a6354368d9387461715
javier <javisalc@gmail.com>
2023-10-31 10:16:53 -0700

    Print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - print all one digit numbers
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{
+for (int number = 0, number <=9; number++)
+{
+printf("%d\n", number);
+{
+return (0);
+}
