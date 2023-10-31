33ccbc15e88a649047d54f9eac1c376d59b6209a
javier <javisalc@gmail.com>
2023-10-31 12:41:34 -0700

    print number combination

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-  int i, f;
-  i = f = '0';
-for (i = '0'; i <= '9'; f++)
+int i, f;
+i = f = '0';
+for (i = '0'; i <= '9'; i++)
 {
-  for (f = '0'; f <= '9'; f++)
-    {
+for (f = '0'; f <= '9'; f++)
+{
 putchar(i);
 putchar(f);
 if ((i != '9') || (i == '9' && f !=9))
