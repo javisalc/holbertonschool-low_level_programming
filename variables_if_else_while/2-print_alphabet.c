af7d21f2f29a178eb62e0f9555eb8e6097c4189d
javier <javisalc@gmail.com>
2023-10-31 05:30:18 -0700

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - a funtion for printing the entire alphabet
+ *       in lowcase
+ *
+ *Return: always 0 for succes
+ */
+int main () {
+char letter = "a";
+while (letter <=z){
+putchar(letter);
+letter++;
+}
+return (0);
+}
