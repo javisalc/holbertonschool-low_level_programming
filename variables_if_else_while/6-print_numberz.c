28979887abdcef168c76e553854486d3a2d3e92d
javier <javisalc@gmail.com>
2023-10-31 10:53:39 -0700

    Print numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,14 +2,14 @@
 /**
  *main - prints single digit number
  *
- *Return - Always (succsess)
+ *Return: Always (succsess)
  */
 int main(void)
 {
-int in;
-for (in = 0; in <= 9; in++)
+int c;
+for (c = '0'; c <= '9'; c++)
 {
-putchar(in);
+putchar(c);
 }
 putchar('\n');
 return (0);
