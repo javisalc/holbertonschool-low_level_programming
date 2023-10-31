8a3f7918e5ac4f7be98fc7bd349f7e6c4d5ed93e
javier <javisalc@gmail.com>
2023-10-31 13:14:18 -0700

    more combinations

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,15 @@ for (i = '0'; i <= '9'; i++)
 {
 for (f = '0'; f <= '9'; f++)
 {
+if (i < f)
+{
 putchar(i);
 putchar(f);
-if ((i != '9') || (i == '9' && f !=9))
+if ((i != '8') || (i == '8' && f != '9'))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
