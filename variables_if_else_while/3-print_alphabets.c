bf2e974e58fce5df2d030034bb12a356ecd9df12
javier <javisalc@gmail.com>
2023-10-31 09:42:00 -0700

    print alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet in lower case and uppercase
+ *
+ * return: Always 0 for success
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'>; letter++)
+{
+putchar(letter);
+}
+for (letter = 'A'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
+  
