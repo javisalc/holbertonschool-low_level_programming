ef9c8bcb7be9eb2bcb3a84e59c743115f7fea0dd
javier <javisalc@gmail.com>
2023-11-01 09:29:00 -0700

    print sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,16 +9,16 @@ int print_sign(int n)
 {
 if (n > '0')
 {
-print_sign('+');
+_putchar('+');
 return (1);
 }
 else if (n == '0')
-print_sign('0');
+_putchar('0');
 return (0);
 }
 else
 {   
-print_sign('-');
+_putchar('-');
 return (-1);
 }
 }
