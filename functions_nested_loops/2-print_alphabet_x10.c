6f6c26a394606c73063372d218bae58b8a4abbb5
javier <javisalc@gmail.com>
2023-10-31 17:30:16 -0700

    alphabet x 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alpabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char a;
+int c = 0;
+while (c <= 9)
+{
+for (a = 'a'; a <= 'z'; z++)
+{
+_putchar(a);
+}
+_putchar('\n');
+i++;
+}
+}
+  
